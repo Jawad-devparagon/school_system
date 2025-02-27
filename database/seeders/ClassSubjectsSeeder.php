990621f09011cccc765c738d00f19cb26126dcc1
Aamish26622 <aamishirfan2662@gmail.com>
2025-02-27 16:21:21 +0500

    Formatting issues has been resolved

--- database/seeders/ClassSubjectsSeeder.php
+++ database/seeders/ClassSubjectsSeeder.php
@@ -4,18 +4,17 @@ namespace Database\Seeders;
 
 use App\Models\SchoolClass;
 use App\Models\Subject;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ClassSubjectsSeeder extends Seeder
 {
     public function run(): void
     {
-         $classes = SchoolClass::all();
-         $subjects = Subject::all();
+        $classes = SchoolClass::all();
+        $subjects = Subject::all();
 
         foreach ($classes as $class) {
-                $class->subjects()->attach($subjects->random(rand(3,5))->pluck('id')->toArray());
-         }
+            $class->subjects()->attach($subjects->random(rand(3, 5))->pluck('id')->toArray());
+        }
     }
 }
--- database/seeders/ClassTeacherSubjectsSeeder.php
+++ database/seeders/ClassTeacherSubjectsSeeder.php
@@ -5,21 +5,19 @@ namespace Database\Seeders;
 use App\Models\SchoolClass;
 use App\Models\Subject;
 use App\Models\Teacher;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ClassTeacherSubjectsSeeder extends Seeder
 {
     public function run(): void
     {
-         $classes = SchoolClass::all();
-         $teachers = Teacher::all()->pluck('id')->toArray();
-         $subjects = Subject::query();
+        $classes = SchoolClass::all();
+        $teachers = Teacher::all()->pluck('id')->toArray();
+        $subjects = Subject::query();
 
-      foreach ($classes as $class)
-      {
-          $class->teachers()->attach($teachers, ['subject_id' => $subjects->inRandomOrder()->limit(1)->value('id')]);
-      }
+        foreach ($classes as $class) {
+            $class->teachers()->attach($teachers, ['subject_id' => $subjects->inRandomOrder()->limit(1)->value('id')]);
+        }
 
     }
 }
